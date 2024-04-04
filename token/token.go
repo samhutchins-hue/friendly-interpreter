801541484f6ef658450146a2f0731c3d208d97a8
Samuel Hutchins <samhutchins123@gmail.com>
2024-04-03 19:07:53 -0700

    updated lexer

--- token/token.go
+++ token/token.go
@@ -18,6 +18,13 @@ const (
   // Operators
   ASSIGN = "="
   PLUS = "+"
+  MINUS = "-"
+  BANG = "!"
+  ASTERISK = "*"
+  SLASH = "/"
+
+  LT = "<"
+  GT = ">"
 
   // Delimiters
   COMMA = ","
@@ -30,4 +37,27 @@ const (
   // Keywords
   FUNCTION = "FUNCTION"
   LET = "LET"
+  TRUE = "TRUE"
+  FALSE = "FALSE"
+  IF = "IF"
+  ELSE = "ELSE"
+  RETURN = "RETURN"
+
 )
+
+var keywords = map[string]TokenType {
+  "fn": FUNCTION,
+  "let": LET,
+  "true": TRUE,
+  "false": FALSE,
+  "if": IF,
+  "else": ELSE,
+  "return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+  if tok, ok := keywords[ident]; ok {
+    return tok
+  }
+  return IDENT
+}
